8ceb467368f848943c56fce3b4c417dd7207967b
Kevin Nguyen <lightandluck@protonmail.com>
2025-04-28 12:15:38 -0700

    Import ImmutableMultiDict

--- open_web_calendar/app.py
+++ open_web_calendar/app.py
@@ -14,6 +14,7 @@ import traceback
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Optional
 from urllib.parse import ParseResult, urlparse, quote
+from werkzeug.datastructures import ImmutableMultiDict
 
 import caldav
 import requests
